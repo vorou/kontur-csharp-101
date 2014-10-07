f8568469b10163e900e1efde3339125e8c22ca4e
vorou <voropaev.viktor@gmail.com>
2014-10-07 14:59:31 +0600

    3 задачи от Марины

---
+++ Section2-BankDeposit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BankDeposit
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input the deposit amount:");
+            double depositAmmount = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please input an interest rate:");
+            double interestRate = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please input time of deposit in months:");
+            int timeOfDeposit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Accumulated sum is:");
+            CalculationOfAccumulatedSum(depositAmmount, interestRate, timeOfDeposit);
+            return;
+        }
+
+
+        static void CalculationOfAccumulatedSum(double depositAmmount, double interestRate, int timeOfDeposit)
+        {
+            double acumulatedSum = depositAmmount*Math.Pow((1 + interestRate/1200), timeOfDeposit);
+            Console.WriteLine(acumulatedSum.ToString());
+            return;
+        }
+    }
+}
---
+++ Section2-BilliardTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Billiards
+{
+	public class BilliardTasks
+	{
+		public double BounceVerticalWall(double directionRadians)
+		{
+		    var BounceVerticalWall = Math.PI - directionRadians;
+			return BounceVerticalWall;
+		}
+		public double BounceHorizontalWall(double directionRadians)
+		{
+		    var BounceHorizontalWall = directionRadians*(-1);
+			return BounceHorizontalWall;
+		}
+
+		public double BounceWall(double directionRadians, double wallInclanationRadians)
+		{
+		    var BounceWall = 2*wallInclanationRadians - directionRadians;
+			return BounceWall;
+		}
+
+	}
+}
