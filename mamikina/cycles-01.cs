ebf59884a4a6bc8e8a383bbbcb8398023e033874
vorou <voropaev.viktor@gmail.com>
2014-10-27 19:52:59 +0500

    Делитель, лабиринт (Мамыкина)

---
+++ mamikina/cycles-01.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Divisor
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = GetNumber();
+           
+            Console.WriteLine(string.Format("Все делители числа {0}:", num));
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    if (i == 1)
+                    {
+                        Console.WriteLine(string.Format("Делитель {0}", i));
+                    }
+
+                    else
+                    {
+                        int maxPow = 0;
+                        for (int j = 1; Math.Pow(i, j) <= num; j++)
+                        {
+                            if (num % Math.Pow(i, j) == 0)
+                            {
+                                if (maxPow < j) maxPow = j;
+                            }
+                        }
+                        Console.WriteLine(string.Format("Делитель {0}, его максимальная степень {1}", i, maxPow));
+                    }
+                    
+                }
+            }
+        }
+
+        static int GetNumber()
+        {
+            Console.WriteLine("Введите число");
+            var numStr = Console.ReadLine();
+            return int.Parse(numStr);
+        }
+
+    }
+}
---
+++ mamikina/cycles-02.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Mazes
+{
+	public static class MazeTasks
+	{
+		/*
+		* Не нужно писать код выхода из произвольного лабиринта. Напишите, решение для конкретных лабиринтов.
+		* Используйте циклы. Ваш К.О.
+		* Постарайтесь решить задачу с ограничение "не более одного цикла на метод".
+		* Подумайте, какие вспомогательные методы помогут сделать ваше решение более лаконичным, понятным и красивым.
+		*/
+
+
+
+		public static void MoveOutFromEmptyMaze(Robot robot, int width, int height)
+		{
+            while (robot.Pos.X < (width - 2) || robot.Pos.Y < (height - 2))
+		    {
+                if (robot.Pos.Y < (height - 2)) robot.MoveTo(Direction.Down);
+                if (robot.Pos.X < (width - 2)) robot.MoveTo(Direction.Right);
+		    }
+           
+		}
+
+		public static void MoveOutFromSnakeMaze(Robot robot, int width, int height)
+		{
+		    var extRight = width - 2;
+		    var extLeft = 1;
+		    var extUp = 1;
+		    var extDown = height - 2;
+            while (robot.Pos.X != extLeft || robot.Pos.Y != extDown)
+            {
+
+                if (robot.Pos.X < extRight && (robot.Pos.Y%4 == 1)) robot.MoveTo(Direction.Right);
+                if ((robot.Pos.X == extRight || robot.Pos.X == extLeft) && (robot.Pos.Y != extDown))
+                {
+                    robot.MoveTo(Direction.Down);
+                    robot.MoveTo(Direction.Down);
+                }
+                if (robot.Pos.X > extLeft && robot.Pos.Y%4 != 1) robot.MoveTo(Direction.Left);
+            }
+
+		}
+
+		public static void MoveOutFromPyramidMaze(Robot robot, int width, int height)
+		{
+            var extRight = width - 2;
+            var extLeft = 1;
+            var extUp = 1;
+            var extDown = height - 2;
+
+            while (robot.Pos.X != 7 || robot.Pos.Y != 1)
+		    {
+                if (robot.Pos.X < extRight && robot.Pos.Y % 4 == 3)
+                robot.MoveTo(Direction.Right);
+                if (robot.Pos.X == extRight || 
+                   (robot.Pos.X == (extLeft + 2) && robot.Pos.Y == 9) ||
+                   (robot.Pos.X == (extRight - 2) && robot.Pos.Y == 7) ||
+                   (robot.Pos.X == (extLeft + 4) && robot.Pos.Y ==5 ) ||
+                   (robot.Pos.X == (extRight - 4) && robot.Pos.Y ==3 ))
+                {
+                    robot.MoveTo(Direction.Up);
+                    robot.MoveTo(Direction.Up);
+                }
+                if (robot.Pos.X > extLeft && robot.Pos.Y % 4 != 3) robot.MoveTo(Direction.Left);
+		    }
+		}
+
+	}
+}
